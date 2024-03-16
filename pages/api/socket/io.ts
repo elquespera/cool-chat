65f5be4b76d8c8d406c87187b1751eb977506bd7
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-03-16 03:24:20 +0100

    feat: broadcast user status

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -24,26 +24,34 @@ export default function handler(
 ) {
   if (!response.socket.server.io) {
     const httpServer: HttpServer = response.socket.server as any;
-    const io = new SocketIOServer<ClientToServerEvents, ServerToClientEvents>(
-      httpServer,
-      {
-        path: process.env.NEXT_PUBLIC_SOCKET_IO_URL!,
-        addTrailingSlash: false,
-        connectionStateRecovery: {},
-      },
-    );
+    const io = new SocketIOServer<
+      ClientToServerEvents,
+      ServerToClientEvents,
+      InterServerEvents,
+      SocketData
+    >(httpServer, {
+      path: process.env.NEXT_PUBLIC_SOCKET_IO_URL!,
+      addTrailingSlash: false,
+      connectionStateRecovery: {},
+    });
 
     io.on("connection", (socket) => {
-      console.log(`Connected to socket ${socket.id}`);
+      socket.broadcast.emit("userStatusChange", socket.data.userId, "online");
 
-      socket.on("disconnect", (reason) => {
-        console.log(
-          `Socket ${socket.id} was disconnected because of ${reason}.`,
+      socket.on("disconnect", () => {
+        socket.broadcast.emit(
+          "userStatusChange",
+          socket.data.userId,
+          "offline",
         );
       });
 
       socket.on("messageModified", (...args) => {
         socket.broadcast.emit("messageModified", ...args);
+      });
+
+      socket.on("userStatusChange", (...args) => {
+        socket.broadcast.emit("userStatusChange", ...args);
       });
     });
 
--- types/socket.d.ts
+++ types/socket.d.ts
@@ -1,13 +1,19 @@
 type MessageModifiedListener = (chatId: string, messageId: string) => void;
+type UserStatus = "online" | "offline" | "typing";
+type UserStatusListener = (userId: string, status: UserStatus) => void;
 
 type ServerToClientEvents = {
   messageModified: MessageModifiedListener;
+  userStatusChange: UserStatusListener;
 };
 
 type ClientToServerEvents = {
   messageModified: MessageModifiedListener;
+  userStatusChange: UserStatusListener;
 };
 
 type InterServerEvents = {};
 
-type SocketData = {};
+type SocketData = {
+  userId: string;
+};
