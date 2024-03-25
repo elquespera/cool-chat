c49edfb3f3068833b90468efbd16f7c48faafaf6
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-03-25 17:38:40 +0100

    fix: remove chat user panel

--- components/room/room.tsx
+++ components/room/room.tsx
@@ -1,13 +1,10 @@
-import { ChatInput } from "@/components/chat/chat-input";
 import { ChatUser } from "@/components/chat/chat-user";
-import { ChatWindow } from "@/components/chat/chat-window";
 import { ChatProvider } from "@/components/providers/chat/chat-provider";
-import { MessageProvider } from "@/components/providers/message/message-provider";
 import { ContactUser } from "@/db/schemas/auth";
 import { ChatSelect } from "@/db/schemas/chats";
 import { EmptyRoom } from "./empty-room";
-import { TextRoom } from "./text-room";
 import { MediaRoom } from "./media-room";
+import { TextRoom } from "./text-room";
 
 type RoomProps = {
   type: "empty" | "text" | "audio" | "video";
@@ -19,13 +16,18 @@ export function Room({ type, interlocutor, chat }: RoomProps) {
   return (
     <ChatProvider interlocutor={interlocutor} chat={chat}>
       {type === "text" ? (
-        <TextRoom />
+        <>
+          <TextRoom />
+          <ChatUser />
+        </>
       ) : type === "empty" ? (
-        <EmptyRoom />
+        <>
+          <EmptyRoom />
+          <ChatUser />
+        </>
       ) : (
         <MediaRoom type={type} />
       )}
-      <ChatUser />
     </ChatProvider>
   );
 }
