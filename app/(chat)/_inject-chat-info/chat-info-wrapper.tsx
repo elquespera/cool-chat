0db6e44e35f8ea7c7c5ae78456cd606d6522ffc9
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-03-26 10:11:46 +0100

    fix: do not use early returns for not ofund  to avoid hook error

--- app/(chat)/_inject-chat-info/chat-info-wrapper.tsx
+++ app/(chat)/_inject-chat-info/chat-info-wrapper.tsx
@@ -1,10 +1,11 @@
+import { NotFound } from "@/components/common/not-found";
 import { routes } from "@/constants/routes";
 import { findChatByIds, getChatById } from "@/db/actions/chats";
 import { getUserById } from "@/db/actions/users";
 import { ContactUser } from "@/db/schemas/auth";
 import { ChatWithUsers } from "@/db/schemas/chats";
 import { getAuth } from "@/lib/auth/get-auth";
-import { notFound, redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 import { InjectChatInfo } from "./inject-chat-info";
 
@@ -23,30 +24,46 @@ export async function ChatInfoWrapper({
 
   let interlocutor: ContactUser | null = null;
   let chat: ChatWithUsers | null = null;
+  let body = children;
 
   if (interlocutorId) {
     const response = await getUserById(interlocutorId);
-    if (!response.ok) notFound();
-
-    interlocutor = response.data;
-    const chatResponse = await findChatByIds(user.id, response.data.id);
-    if (chatResponse.ok) redirect(`${routes.chat}/${chatResponse.data.id}`);
+    if (response.ok) {
+      interlocutor = response.data;
+      const chatResponse = await findChatByIds(user.id, response.data.id);
+      if (chatResponse.ok) redirect(`${routes.chat}/${chatResponse.data.id}`);
+    } else {
+      body = UserNotFound;
+    }
   }
 
   if (chatId) {
     const response = await getChatById(chatId);
-    if (!response.ok) notFound();
-    chat = response.data;
+    if (response.ok) {
+      chat = response.data;
 
-    if (chat.userOneId !== user.id && chat.userTwoId !== user.id) notFound();
-
-    interlocutor = chat.userOneId === user.id ? chat.userTwo : chat.userOne;
+      if (chat.userOneId === user.id || chat.userTwoId === user.id) {
+        interlocutor = chat.userOneId === user.id ? chat.userTwo : chat.userOne;
+      } else {
+        body = ChatNotFound;
+      }
+    } else {
+      body = ChatNotFound;
+    }
   }
 
   return (
     <>
       <InjectChatInfo chat={chat} interlocutor={interlocutor} />
-      {children}
+      {body}
     </>
   );
 }
+
+const UserNotFound = (
+  <NotFound title="User Not Found">{`The user you're looking for was not found on the server.`}</NotFound>
+);
+
+const ChatNotFound = (
+  <NotFound title="Chat Not Found">{`The chat you're looking for was not found on the server.`}</NotFound>
+);
