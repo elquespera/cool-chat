9a150913124313c6a703bdebcae0c31c8f69ac19
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-03-29 09:34:45 +0100

    chore: update deps

--- app/attachments/[file]/route.ts
+++ app/attachments/[file]/route.ts
@@ -9,10 +9,13 @@ export const GET = async (
 ) => {
   try {
     const filePath = path.join(process.cwd(), attachmentDir, file);
+    console.log(`Reading file ${filePath}`);
+
     const result = await readFile(filePath);
 
     return new Response(result, { status: 200 });
-  } catch {
+  } catch (error) {
+    console.log(String(error));
     new Response(JSON.stringify({ error: `Failed to read file ${file}` }), {
       status: 500,
     });
