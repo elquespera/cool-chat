3c43b32b2d54c9f01a1166e858389f612819477f
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-03-30 04:17:52 +0100

    feat: improve assistant welcome layout

--- components/assistant/assistant-welcome.tsx
+++ components/assistant/assistant-welcome.tsx
@@ -44,10 +44,10 @@ export function AssistantWelcome() {
 
   return (
     <div className="flex grow flex-col items-center justify-center p-4 @container">
-      <h2 className="mb-2 select-none text-center text-3xl font-semibold uppercase tracking-tighter">
+      <h2 className="mb-2 select-none text-center text-3xl font-semibold uppercase tracking-tighter @lg:text-4xl">
         Choose a Model
       </h2>
-      <p className="mb-8 select-none font-medium text-muted-foreground">
+      <p className="mb-8 select-none font-semibold text-muted-foreground">
         Chat with an AI Assistant
       </p>
       <div className="flex max-w-lg flex-col gap-8 rounded-lg border bg-card p-6 shadow-md @lg:p-8">
@@ -55,14 +55,12 @@ export function AssistantWelcome() {
           assistant={assistantType}
           onAssistantChange={handleAssistantChange}
         />
-        <div className="flex gap-4">
-          <UserAvatar className="h-20 w-20" avatarUrl={assistant.avatarUrl} />
-          <div className="flex min-h-44 flex-col">
-            <h3 className="text-xl font-semibold">{assistant.username}</h3>
-            <p className="mb-6 mt-2 text-sm font-medium text-muted-foreground">
-              {assistant.description}
-            </p>
-          </div>
+        <div className="flex flex-col items-center gap-4">
+          <UserAvatar className="h-32 w-32" avatarUrl={assistant.avatarUrl} />
+          <h3 className="text-xl font-semibold">{assistant.username}</h3>
+          <p className="mb-6 mt-2 min-h-20 text-sm font-medium text-muted-foreground">
+            {assistant.description}
+          </p>
         </div>
         <div className="mt-auto flex flex-wrap justify-between gap-2">
           <IconButton variant="link" href={assistant.url}>
