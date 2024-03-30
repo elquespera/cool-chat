1ebbfeacb26fc63d8f00b14b1dacbf97c4f67258
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-03-30 04:26:01 +0100

    feat: add assistant button animation

--- components/contact/assistant-button.tsx
+++ components/contact/assistant-button.tsx
@@ -8,7 +8,7 @@ export function AssistantButton() {
   const { isScrolledDown } = useContactScroll();
 
   return (
-    <div className="absolute inset-y-0 -right-4 flex items-center bg-gradient-to-l from-background via-background to-transparent pe-4 ps-8">
+    <div className="absolute inset-y-0 -right-4 flex items-center bg-gradient-to-l from-background via-background to-transparent pe-4 ps-12">
       <IconButton
         href={`${routes.assistant}`}
         aria-label="Assistant"
@@ -16,12 +16,15 @@ export function AssistantButton() {
         toolTipSide="left"
         toolTipOffset={10}
         className={cn(
-          "h-10 w-10 shrink-0 rounded-full transition-all hover:scale-110 hover:shadow-xl",
+          "group h-10 w-10 shrink-0 rounded-full transition-all hover:scale-110 hover:shadow-xl",
           isScrolledDown && "h-8 w-8 -translate-y-5",
         )}
         icon={
           <AssistantIcon
-            className={cn("h-6 w-6", isScrolledDown && "h-4 w-4")}
+            className={cn(
+              "h-6 w-6 transition-transform delay-200 group-hover:rotate-180",
+              isScrolledDown && "h-4 w-4",
+            )}
           />
         }
       />
