8333a4d65f32b50209014b2acffec9487c29e55a
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-03-30 04:07:16 +0100

    feat: animate assistant button

--- components/contact/assistant-button.tsx
+++ components/contact/assistant-button.tsx
@@ -8,18 +8,23 @@ export function AssistantButton() {
   const { isScrolledDown } = useContactScroll();
 
   return (
-    <IconButton
-      href={`${routes.assistant}`}
-      aria-label="Assistant"
-      toolTip="Assistant"
-      toolTipSide="left"
-      className={cn(
-        "absolute right-4 h-10 w-10 shrink-0 rounded-full transition-all",
-        isScrolledDown && "h-8 w-8 -translate-y-5",
-      )}
-      icon={
-        <AssistantIcon className={cn("h-6 w-6", isScrolledDown && "h-4 w-4")} />
-      }
-    />
+    <div className="absolute inset-y-0 -right-4 flex items-center bg-gradient-to-l from-background via-background to-transparent pe-4 ps-8">
+      <IconButton
+        href={`${routes.assistant}`}
+        aria-label="Assistant"
+        toolTip="Assistant"
+        toolTipSide="left"
+        toolTipOffset={10}
+        className={cn(
+          "h-10 w-10 shrink-0 rounded-full transition-all hover:scale-110 hover:shadow-xl",
+          isScrolledDown && "h-8 w-8 -translate-y-5",
+        )}
+        icon={
+          <AssistantIcon
+            className={cn("h-6 w-6", isScrolledDown && "h-4 w-4")}
+          />
+        }
+      />
+    </div>
   );
 }
