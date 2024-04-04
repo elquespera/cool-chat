21fedb739c98324f1569849632424d5ce49044b9
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-04-04 12:30:19 +0200

    feat: implement random user funny names

--- lib/auth/sign-up-as-anonymous.ts
+++ lib/auth/sign-up-as-anonymous.ts
@@ -5,16 +5,13 @@ import { addUserWithoutAuth as addUser } from "@/db/actions/users";
 import { LibsqlError } from "@libsql/client";
 import { redirect } from "next/navigation";
 import { createSession } from "./session";
-import { randomUsername } from "../random/random-username";
-import { randomEmail } from "../random/random-email";
+import { randomUser } from "@/lib/random/random-user";
 
 export async function signUpAsAnonymous(
   redirectURI: string = routes.home,
 ): Promise<AuthActionResult> {
   try {
-    const email = randomEmail();
-    const username = randomUsername();
-    const user = await addUser({ email, username, role: "anonymous" });
+    const user = await addUser({ ...randomUser(), role: "anonymous" });
 
     if (!user) throw new Error("Can't add user.");
     await createSession(user.id);
--- lib/random/random-email.ts
+++
@@ -1,3 +0,0 @@
-import { randomInt } from "./random-int";
-
-export const randomEmail = () => `test${randomInt(20, 1000)}@mail.com`;
